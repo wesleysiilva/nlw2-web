5bf7616aa3dc0022403b32f479e27e32a79cee0c
Wesley Silva <wesleysa95@gmail.com>
2020-08-03 23:59:07 -0300

    Study page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -23,14 +24,15 @@ function Landing(){
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aula"/>
             Dar aulas
-          </a>          
+          </Link>          
         </div>
 
         <span className="total-connections">
