fab36bb78e4d17fd8d78d0ea87337ffac67ff24b
Wesley Silva <wesleysa95@gmail.com>
2020-08-05 23:46:20 -0300

    Integrando página de listagem de professores com backend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,27 +2,37 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher{
+  avatar: string
+  bio: string
+  cost: number
+  id: number
+  name: string
+  subject: string    
+  whatsapp: string
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/49622743?s=460&u=f667671c8eacbd89563d79a83d9d81686ce15b84&v=4" alt="Wesley Silva"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Wesley Silva</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br/><br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.            
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button>
           <img src={whatsappIcon} alt="WhatsApp"/>
